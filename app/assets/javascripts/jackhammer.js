a90525807f60f3cafa5c1c8f96a0c00c6ad59bfb
JoeMugwaneza <muganezajoseph@gmail.com>
2017-02-23 13:30:05 +0200

    completed jackhammer exercises (clicking each box and get different color)

--- app/assets/javascripts/jackhammer.js
+++ app/assets/javascripts/jackhammer.js
@@ -4,8 +4,17 @@
 //     alert("Success !!!");
 //   }
 
-var JackHammerClicked = 1
+var JackHammerClicked = 1;
+
+var colors = ["yellow", "black", "indigo", "blue", "green"];
+var i = 0
+
 function jackHammer() {
+
+  var colorChange = document.getElementById("color-on-click"); 
+  colorChange.style["background-color"] = colors[i]; 
+  i += 1
+  
   if (JackHammerClicked === 5) {
     alert("Success !!!!!");
   } else {
